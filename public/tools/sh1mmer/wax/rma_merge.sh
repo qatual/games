8f1679da5668613b354d0fc9b36d36937e3fa97f
qatual <central@soloo.win>
2025-07-15 16:56:18 -0400

    hi

--- public/tools/sh1mmer/wax/rma_merge.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# use rma_merge.sh --help
-# todo: make this script standalone
-
-chmod +x lib/py/image_tool.py
-lib/py/image_tool.py rma merge "$@"
--- public/tools/sh1mmer/wax/wax_macos.sh
+++
@@ -1,2 +0,0 @@
-echo "wax_macos.sh is deprecated. Please use wax4web or a virtual machine."
-exit 1
